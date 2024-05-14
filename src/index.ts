57ac3f57042c70c412ca5ad815d057121b9d5ebc
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-05-14 13:56:46 +0700

    implement get all books

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { bookList } from './data/books'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Book Finder 📚')
+})
+
+app.get('/books', (c) => {
+  return c.json(bookList)
 })
 
 export default app
