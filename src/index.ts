ac4ec4c53a7f1c0f95e1d6819f89bd53d4bd516d
yusufvalid <103932140+yusufvalid@users.noreply.github.com>
2024-05-14 13:57:09 +0700

    implement get detail book by id

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,17 @@ app.get('/books', (c) => {
   return c.json(bookList)
 })
 
+app.get('/books/:id', (c) => {
+  const id = Number(c.req.param('id'))
+
+  const book = bookList.find((book) => book.id === id)
+
+  if (!book) {
+    c.status(404)
+    return c.json({ message: 'Book not found' })
+  }
+
+  return c.json(book)
+})
+
 export default app
